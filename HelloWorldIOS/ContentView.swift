4c3cc2c60a35bbd33e67ddb6e9e5ee7caa50b0dd
Dennis Dachkovski <dennis@dachkovski.de>
2026-05-10 17:13:40 +0200

    Initial Commit

---
+++ HelloWorldIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWorldIOS
+//
+//  Created by Dennis Dachkovski on 10.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWorldIOS/HelloWorldIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldIOSApp.swift
+//  HelloWorldIOS
+//
+//  Created by Dennis Dachkovski on 10.05.26.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
